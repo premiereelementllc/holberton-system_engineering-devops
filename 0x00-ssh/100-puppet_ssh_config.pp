01dfc2e2f4f88d0b7a7bea0aa7ce233667e95f6a
premiereelementllc <91158034+premiereelementllc@users.noreply.github.com>
2022-07-14 22:18:12 -0500

    Update 100-puppet_ssh_config.pp

---
+++ 0x00-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+
+
+
+include stdlib
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+ ensure => 'present',
+ path    => '/etc/ssh/ssh_config',
+ line    => '    PasswordAuthentication no',
+ replace => true,
+}
